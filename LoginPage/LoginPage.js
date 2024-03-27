634c3a505b20959357f059608d808bec9d330186
Sandrik <156957488+Sandrik@users.noreply.github.com>
2024-03-27 08:07:09 -0400

    Update LoginPage.js

--- LoginPage/LoginPage.js
+++ LoginPage/LoginPage.js
@@ -6,11 +6,14 @@
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-    <div className="container">
+    <div class="container">
         <form id="loginForm" action="login.php" method="POST">
             <h2>User Login</h2>
             <input type="email" name="email" placeholder="Email" required />
             <input type="password" name="password" placeholder="Password" required />
+            <label>
+                <input type="checkbox" name="rememberMe" /> Remember Me
+            </label>
             <button type="submit">Login</button>
             <a href="register.html">New User? Register Here</a>
             <a href="forgot_password.html">Forgot Password?</a>
@@ -20,7 +23,6 @@
     <script src="script.js"></script>
 </body>
 </html>
-
 
 
 
